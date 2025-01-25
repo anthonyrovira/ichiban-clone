6d912398a88f844d734e1dfe6799b33b7c3a7774
Anthony <anthonyrov@gmail.com>
2025-01-25 20:38:56 +0100

    add: third section

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -21,3 +21,15 @@ export function generateTransmissionFrameUrls(totalFrames: number) {
     return `/frames/transmission/img-${frameNumber}.webp`;
   });
 }
+
+/**
+ * Generate frame urls
+ * @param totalFrames - number of frames
+ * @returns array of frame urls
+ */
+export function generateHeartFrameUrls(totalFrames: number) {
+  return Array.from({ length: totalFrames }, (_, i) => {
+    const frameNumber = `${(i + 1).toString().padStart(3, "0")}`;
+    return `/frames/heart/img-${frameNumber}.webp`;
+  });
+}
