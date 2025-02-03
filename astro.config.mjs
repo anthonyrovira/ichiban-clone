802dae28a25594176e44c5608c21b56b65bfb32a
Anthony <anthonyrov@gmail.com>
2025-02-03 21:06:00 +0100

    fix: update Vercel adapter import and add .vercel to .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,5 +22,5 @@ export default defineConfig({
     },
   },
 
-  adapter: vercel(),
+  adapter: vercel({}),
 });
