ed8adebd5a6b013f0ccf2f693826a520590f7a6b
Anthony <anthonyrov@gmail.com>
2025-02-03 19:01:00 +0100

    update: reorganize assets

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -6,7 +6,7 @@
 export function generateHeroFrameUrls(totalFrames: number) {
   return Array.from({ length: totalFrames }, (_, i) => {
     const frameNumber = `${(i + 1).toString().padStart(3, "0")}`;
-    return `/frames/hero/moto-${frameNumber}.webp`;
+    return `/src/assets/frames/hero/moto-${frameNumber}.webp`;
   });
 }
 
@@ -18,18 +18,18 @@ export function generateHeroFrameUrls(totalFrames: number) {
 export function generateTransmissionFrameUrls(totalFrames: number) {
   return Array.from({ length: totalFrames }, (_, i) => {
     const frameNumber = `${(i + 1).toString().padStart(3, "0")}`;
-    return `/frames/transmission/img-${frameNumber}.webp`;
+    return `/src/assets/frames/transmission/img-${frameNumber}.webp`;
   });
 }
 
 /**
  * Generate frame urls
- * @param totalFrames - number of frames
+ * /srcparam totalFrames - number of frames
  * @returns array of frame urls
  */
 export function generateHeartFrameUrls(totalFrames: number) {
   return Array.from({ length: totalFrames }, (_, i) => {
     const frameNumber = `${(i + 1).toString().padStart(3, "0")}`;
-    return `/frames/heart/img-${frameNumber}.webp`;
+    return `/src/assets/frames/heart/img-${frameNumber}.webp`;
   });
 }
